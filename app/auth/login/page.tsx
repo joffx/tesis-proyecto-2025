937d2145eb487a532b70c3770fca4f81e8d4c119
Joffre Veloz <joffre.veloz@fastery.dev>
2025-01-07 16:07:56 -0500

    Update page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,4 +1,3 @@
-import { GalleryVerticalEnd } from "lucide-react";
 import Image from "next/image";
 
 import { LoginForm } from "@/components/login-form";
@@ -7,7 +6,13 @@ export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <Image src={"/logoups.svg"} alt={"logoups"} width={150} height={150} className="mx-auto" />
+        <Image
+          src={"/logoups.svg"}
+          alt={"logoups"}
+          width={150}
+          height={150}
+          className="mx-auto"
+        />
         <a
           href="/"
           className="flex items-center gap-2 font-semibold text-center"
