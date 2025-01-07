76f5d3c479835bc0635787220b5a77d24b4400c4
Joffre Veloz <joffre.veloz@fastery.dev>
2025-01-07 16:05:01 -0500

    add auth/login

---
+++ components/login-form.tsx
@@ -0,0 +1,66 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { DoorOpenIcon } from "lucide-react";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Iniciar Sesión</CardTitle>
+          <CardDescription>
+            Ingresa tus credenciales para continuar
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid gap-6">
+              <div className="grid gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Correo</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="jvelozp@est.ups.edu.ec"
+                    value={"jvelozp@est.ups.edu.ec"}
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Contraseña</Label>
+                  </div>
+                  <Input
+                    id="password"
+                    type="password"
+                    value={"Abc123"}
+                    required
+                  />
+                </div>
+                <Button type="submit" className="w-full">
+                  <DoorOpenIcon className="w-4 h-4" />
+                  <span>Iniciar Sesión</span>
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+        Autor: <a href="#">Joffre Andres Veloz Pazmiño</a>{" "}
+      </div>
+    </div>
+  );
+}
