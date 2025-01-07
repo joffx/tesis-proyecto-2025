ca13a500f950e305f8bf15a44a06438cfa8965be
Joffre Veloz <joffre.veloz@fastery.dev>
2025-01-07 16:44:39 -0500

    fix auth/login

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,27 +1,7 @@
-import Image from "next/image";
-
-import { LoginForm } from "@/components/login-form";
-
-export default function LoginPage() {
+export default function DasboardPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <Image
-          src={"/logoups.svg"}
-          alt={"logoups"}
-          width={150}
-          height={150}
-          className="mx-auto"
-        />
-        <a
-          href="/"
-          className="flex items-center gap-2 font-semibold text-center"
-        >
-          “DESARROLLO DE UN SISTEMA DE ALARMA Y DETECCIÓN INTELIGENTE DE ARMAS
-          DE FUEGO MEDIANTE ANÁLISIS DE VIDEO CON TECNOLOGÍA IOT”
-        </a>
-        <LoginForm />
-      </div>
+    <div>
+      <h1>Dashboard</h1>
     </div>
   );
 }
--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -12,28 +12,27 @@ import { Label } from "@/components/ui/label";
 import { DoorOpenIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { toast } from "sonner";
 
-export function LoginForm({
-  className,
-  ...props
-}: React.ComponentPropsWithoutRef<"div">) {
+export function LoginForm() {
+  const router = useRouter();
   const [bodylogin, setBodyLogin] = useState({
-    email: "",
-    password: "",
+    email: "jvelozp@est.ups.edu.ec",
+    password: "Abc123",
   });
 
   const loginApi = (email: string, password: string) => {
+    console.log("test" + email + password);
     if (email === "jvelozp@est.ups.edu.ec" && password === "Abc123") {
+      toast.success("Credenciales correctas");
       router.push("/dashboard");
-    }else{
-      tona
+    } else {
+      toast.error("Credenciales incorrectas");
     }
-    
   };
 
-  const router = useRouter();
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
+    <div>
       <Card>
         <CardHeader className="text-center">
           <CardTitle className="text-xl">Iniciar Sesión</CardTitle>
@@ -42,37 +41,43 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <div className="grid gap-6">
             <div className="grid gap-6">
-              <div className="grid gap-6">
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Correo</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="jvelozp@est.ups.edu.ec"
-                    value={"jvelozp@est.ups.edu.ec"}
-                    required
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Contraseña</Label>
-                  </div>
-                  <Input
-                    id="password"
-                    type="password"
-                    value={"Abc123"}
-                    required
-                  />
-                </div>
-                <Button onClick={} type="submit" className="w-full">
-                  <DoorOpenIcon className="w-4 h-4" />
-                  <span>Iniciar Sesión</span>
-                </Button>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Correo</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  value={bodylogin.email}
+                  onChange={(e) =>
+                    setBodyLogin({ ...bodylogin, email: e.target.value })
+                  }
+                  required
+                />
               </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Contraseña</Label>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  value={bodylogin.password}
+                  onChange={(e) =>
+                    setBodyLogin({ ...bodylogin, password: e.target.value })
+                  }
+                  required
+                />
+              </div>
+              <Button
+                onClick={() => loginApi(bodylogin.email, bodylogin.password)}
+                className="w-full"
+              >
+                <DoorOpenIcon className="w-4 h-4" />
+                <span>Iniciar Sesión</span>
+              </Button>
             </div>
-          </form>
+          </div>
         </CardContent>
       </Card>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
